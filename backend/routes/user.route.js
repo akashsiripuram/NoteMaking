1769d68fdb8e0558b60a24ac65095832fdb08593
suraj pro <71000909+suraj719@users.noreply.github.com>
2025-02-26 13:31:24 +0530

    Feat: signup

---
+++ backend/routes/user.route.js
@@ -0,0 +1,7 @@
+const { createUser } = require("../controllers/user.controller");
+
+const router = require("express").Router();
+
+router.post("/signup", createUser);
+
+module.exports = router;
--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -1,8 +1,59 @@
-export default function Signup(){
-    return(
-        <div>
-            <h2>Signup</h2>
-            {/* Add your login form here */}
-        </div>
-    )
+import { useState } from "react";
+import axios from "axios";
+export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function createAccount(e) {
+    e.preventDefault();
+    try {
+      const data = await axios.post("http://localhost:5000/api/signup", {
+        username: username,
+        email: email,
+        password: password,
+      });
+      console.log(data);    
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={createAccount}>
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="border-2 border-black"
+          type="text"
+          placeholder="username"
+        />
+        <br />
+        <input
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          className="border-2 border-black"
+          type="email"
+          placeholder="email"
+        />
+        <br />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          className="border-2 border-black"
+          type="password"
+          placeholder="password"
+        />
+        <br />
+        <button className="bg-green-400 p-2 rounded-sm" type="submit">
+          create account
+        </button>
+      </form>
+    </div>
+  );
 }
